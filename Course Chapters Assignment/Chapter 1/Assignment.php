909b2aafb834f1b2ee764aa9d07114217a43fb85
546ian <ianwaweru38@gmail.com>
2024-10-12 08:40:47 +0300

    ass.1

---
+++ Course Chapters Assignment/Chapter 1/Assignment.php
@@ -0,0 +1,11 @@
+<?php
+
+echo <h1>Student Information</h1>
+
+echo <p><b>"Roll Number: "</b> "CIT-227-177/2022"</p>
+echo <p><b>"Course Name: "</b>"Software Engineering"</p>
+echo <p><b>"Technology Name: "</b>"WEB II"</p>
+
+
+
+?>
